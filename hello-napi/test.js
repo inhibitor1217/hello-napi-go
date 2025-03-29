4e6cb91b8432aca66ac8a40871d3fefc2557f964
inhibitor1217 <inhibitor@kaist.ac.kr>
2025-03-29 18:41:33 +0900

    feat: add method

--- hello-napi/test.js
+++ hello-napi/test.js
@@ -6,3 +6,9 @@ assert.ok(typeof hello.hello === 'function', 'hello should be a function');
 assert.doesNotThrow(() => {
   hello.hello();
 }, Error, 'hello should not throw an error');
+
+assert.ok(typeof hello.add === 'function', 'add should be a function');
+assert.doesNotThrow(() => {
+  hello.add(1, 2);
+}, Error, 'add should not throw an error');
+assert.equal(hello.add(1, 2), 3, 'add should return 3');
--- src/hello.cc
+++ src/hello.cc
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <napi.h>
 
-void Method(const Napi::CallbackInfo& info) {
+void Hello(const Napi::CallbackInfo& info) {
     printf("Hello, world!\n");
 }
 
+Napi::Number Add(const Napi::CallbackInfo& info) {
+    Napi::Number a = info[0].As<Napi::Number>();
+    Napi::Number b = info[1].As<Napi::Number>();
+    return Napi::Number::New(info.Env(), a.Int32Value() + b.Int32Value());
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Method));
+    exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Hello));
+    exports.Set(Napi::String::New(env, "add"), Napi::Function::New(env, Add));
     return exports;
 }
 
